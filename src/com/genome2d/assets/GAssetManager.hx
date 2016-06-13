74018cf536554c17d0fe94f7dd048a97bfac650f
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-06-13 17:47:26 +0200

    update

--- src/com/genome2d/assets/GAssetManager.hx
+++ src/com/genome2d/assets/GAssetManager.hx
@@ -63,6 +63,10 @@ class GAssetManager {
     static public function getXmlAssetById(p_id:String):GXmlAsset {
         return cast g2d_references.get(p_id);
     }
+	
+	static public function getTextAssetById(p_id:String):GTextAsset {
+		return cast g2d_references.get(p_id);
+	}
 
     static public function getImageAssetById(p_id:String):GImageAsset {
         return cast g2d_references.get(p_id);
@@ -74,6 +78,8 @@ class GAssetManager {
                 return new GImageAsset(p_url, p_id);
             case "xml" | "fnt":
                 return new GXmlAsset(p_url, p_id);
+			case "fbx":
+				return new GTextAsset(p_url, p_id);
         }
 
         return null;
