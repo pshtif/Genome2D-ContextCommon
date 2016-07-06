60d0bda7e9158102c0998f8bf195e99c5490a796
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-07-06 15:14:39 +0200

    update debug INFO now doesn't need debug compile flag

--- src/com/genome2d/debug/GDebug.hx
+++ src/com/genome2d/debug/GDebug.hx
@@ -44,11 +44,9 @@ class GDebug {
     }
 
     inline static public function info(?p_arg1:Dynamic, ?p_arg2:Dynamic, ?p_arg3:Dynamic, ?p_arg4:Dynamic, ?p_arg5:Dynamic, ?p_arg6:Dynamic, ?p_arg7:Dynamic, ?p_arg8:Dynamic, ?p_arg9:Dynamic, ?p_arg10:Dynamic, ?p_arg11:Dynamic, ?p_arg12:Dynamic, ?p_arg13:Dynamic, ?p_arg14:Dynamic, ?p_arg15:Dynamic, ?p_arg16:Dynamic, ?p_arg17:Dynamic, ?p_arg18:Dynamic, ?p_arg19:Dynamic, ?p_arg20:Dynamic, ?pos:PosInfos):Void {
-        #if genome_debug
         if (showPriority <= GDebugPriority.INFO) {
             g2d_internal(GDebugPriority.INFO, pos, p_arg1, p_arg2, p_arg3, p_arg4, p_arg5, p_arg6, p_arg7, p_arg8, p_arg9, p_arg10, p_arg11, p_arg12, p_arg13, p_arg14, p_arg15, p_arg16, p_arg17, p_arg18, p_arg19, p_arg20);
         }
-        #end
     }
 	
 	inline static public function editor(?p_arg1:Dynamic, ?p_arg2:Dynamic, ?p_arg3:Dynamic, ?p_arg4:Dynamic, ?p_arg5:Dynamic, ?p_arg6:Dynamic, ?p_arg7:Dynamic, ?p_arg8:Dynamic, ?p_arg9:Dynamic, ?p_arg10:Dynamic, ?p_arg11:Dynamic, ?p_arg12:Dynamic, ?p_arg13:Dynamic, ?p_arg14:Dynamic, ?p_arg15:Dynamic, ?p_arg16:Dynamic, ?p_arg17:Dynamic, ?p_arg18:Dynamic, ?p_arg19:Dynamic, ?p_arg20:Dynamic, ?pos:PosInfos):Void {
@@ -125,8 +123,8 @@ class GDebug {
 				"";
         }
         if (p_pos != null) msg += p_pos.fileName+":"+p_pos.lineNumber+" : "+p_pos.methodName;
-        if (p_args.length>0) msg += " : " + p_args.toString();
-
+        if (p_args.length > 0) msg += " : " + p_args.toString();
+		
         GDebug.trace(msg);
         if (p_priority == GDebugPriority.ERROR || p_priority == GDebugPriority.G2D_ERROR) throw msg;
     }
