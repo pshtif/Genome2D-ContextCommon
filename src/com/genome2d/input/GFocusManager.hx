53d49de650e31bc9df932da6c1bd87388405dc18
Peter @sHTiF Stefcek <pshtif@gmail.com>
2016-10-29 15:43:09 +0200

    focus manager

--- src/com/genome2d/input/GFocusManager.hx
+++ src/com/genome2d/input/GFocusManager.hx
@@ -1,15 +1,27 @@
+/*
+ * 	Genome2D - 2D GPU Framework
+ * 	http://www.genome2d.com
+ *
+ *	Copyright 2011-2014 Peter Stefcek. All rights reserved.
+ *
+ *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
+ */
+
 package com.genome2d.input;
 import com.genome2d.input.IGInteractive;
 
-/**
- * ...
- * @author 
- */
 class GFocusManager
 {
-	static public var activeFocus:IGInteractive;
-	
-	static public function setFocus(p_interactive:IGInteractive):Void {
-		activeFocus = p_interactive;
-	}	
+	static private var g2d_focusInstance:IGInteractive;
+
+	static public function hasFocus(p_focusInstance:IGInteractive):Bool {
+		return g2d_focusInstance == p_focusInstance;
+	}
+
+	@:access(com.genome2d.input.IGInteractive)
+	static public function setFocus(p_focusInstance:IGInteractive):Void {
+		if (g2d_focusInstance != null) g2d_focusInstance.lostFocus();
+		g2d_focusInstance = p_focusInstance;
+		if (g2d_focusInstance != null) g2d_focusInstance.gotFocus();
+	}
 }
---
+++ src/com/genome2d/input/IGInteractive.hx
@@ -0,0 +1,12 @@
+package com.genome2d.input;
+
+/**
+ * @author Peter @sHTiF Stefcek
+ */
+
+interface IGInteractive
+{
+    private function gotFocus():Void;
+
+    private function lostFocus():Void;
+}
