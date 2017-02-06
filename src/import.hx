78fce1e613660c14ccfafc0606286469989bfcfc
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-02-06 22:59:50 +0100

    specified global imports

---
+++ src/import.hx
@@ -0,0 +1,9 @@
+#if !macro
+import com.genome2d.debug.GDebug;
+import com.genome2d.macros.MGDebug;
+import com.genome2d.callbacks.GCallback.GCallback0;
+import com.genome2d.callbacks.GCallback.GCallback1;
+import com.genome2d.callbacks.GCallback.GCallback2;
+import com.genome2d.callbacks.GCallback.GCallback3;
+import com.genome2d.tween.GTween;
+#end
