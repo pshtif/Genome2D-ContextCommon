6df540666b79cfdb332c5516554b42c03387e2ac
Peter @sHTiF Stefcek <pshtif@gmail.com>
2017-07-12 13:17:21 +0200

    initial WebGL filter support

--- src/com/genome2d/context/IGRenderer.hx
+++ src/com/genome2d/context/IGRenderer.hx
@@ -7,9 +7,16 @@
  *	License:: ./doc/LICENSE.md (https://github.com/pshtif/Genome2D/blob/master/LICENSE.md)
  */
 package com.genome2d.context;
+#if js
+import js.html.webgl.Program;
+#end
 
 interface IGRenderer
 {
+	#if js
+	function getProgram():Program;
+	#end
+
     function bind(p_context:IGContext, p_reinitialize:Int):Void;
 	
 	function push():Void;
